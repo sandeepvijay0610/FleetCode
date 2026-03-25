a3cc07977c502f4b9e98bb5b23f2ca91116cfb72
Sandeep VIjay <sandeepvijay0610@gmail.com>
2026-03-26 02:56:32 +0530

    Delete Fleetcode_Backend/FleetCode-main directory

--- Fleetcode_Backend/FleetCode-main/models/squad.py
+++
@@ -1,9 +0,0 @@
-from mongoengine import Document, StringField, IntField, DateTimeField
-from datetime import datetime
-
-class Squad(Document):
-    squad_name = StringField(required=True, unique=True)
-    score = IntField(default=0)
-    streak = IntField(default=0)
-    spotter_tokens = IntField(default=1)
-    created_at = DateTimeField(default=datetime.utcnow)
